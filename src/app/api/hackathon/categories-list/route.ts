6acc7f23da2e0454e5cab6ba5a05f0f63df27781
shikoto <1sh1m1da1@gmail.com>
2023-09-23 11:05:20 +0000

    fjladksfjkljl

--- src/app/api/hackathon/categories-list/route.ts
+++ src/app/api/hackathon/categories-list/route.ts
@@ -1,26 +1,40 @@
 import Categories from "@/Models/Categories";
 import connectMongo from "@/utils/connectMongo";
 
+// export async function GET(req: Request) {
+//     try {
+//         await connectMongo();
+
+//         const categories = await Categories.find();
+
+//         // if(categories) {
+//         //     return new Response(JSON.stringify({categories, succeeded: true}), {
+//         //         status: 200
+//         //     })
+//         // }else {
+//         //     return new Response(JSON.stringify({msg: 'There has been an error please try again later', succeeded: false}), {
+//         //         status: 400
+//         //     })
+//         // }
+
+//         return new Response(JSON.stringify({categories, succeeded: true}), {
+//             status: 200
+//         })
+//     } catch(err) {
+//         console.log(err)
+//     }
+// }
+
 export async function GET(req: Request) {
     try {
         await connectMongo();
 
         const categories = await Categories.find();
 
-        // if(categories) {
-        //     return new Response(JSON.stringify({categories, succeeded: true}), {
-        //         status: 200
-        //     })
-        // }else {
-        //     return new Response(JSON.stringify({msg: 'There has been an error please try again later', succeeded: false}), {
-        //         status: 400
-        //     })
-        // }
-
         return new Response(JSON.stringify({categories, succeeded: true}), {
             status: 200
         })
-    } catch(err) {
+    }catch(err) {
         console.log(err)
     }
 }
