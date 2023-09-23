5a2be5c974d160cb0869cd8c216c63cde4648fb0
shikoto <1sh1m1da1@gmail.com>
2023-09-23 10:59:33 +0000

    fjkdsl;ajfkl

--- src/app/api/hackathon/categories-list/route.ts
+++ src/app/api/hackathon/categories-list/route.ts
@@ -5,17 +5,21 @@ export async function GET(req: Request) {
     try {
         await connectMongo();
 
-        const categories = await Categories.find({});
+        const categories = await Categories.find();
 
-        if(categories) {
-            return new Response(JSON.stringify({categories, succeeded: true}), {
-                status: 200
-            })
-        }else {
-            return new Response(JSON.stringify({msg: 'There has been an error please try again later', succeeded: false}), {
-                status: 400
-            })
-        }
+        // if(categories) {
+        //     return new Response(JSON.stringify({categories, succeeded: true}), {
+        //         status: 200
+        //     })
+        // }else {
+        //     return new Response(JSON.stringify({msg: 'There has been an error please try again later', succeeded: false}), {
+        //         status: 400
+        //     })
+        // }
+
+        return new Response(JSON.stringify({categories, succeeded: true}), {
+            status: 200
+        })
     } catch(err) {
         console.log(err)
     }
