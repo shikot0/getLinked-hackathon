02e696b1ba07d131db4c3c54eb96d7be4dd61b34
shikoto <1sh1m1da1@gmail.com>
2023-09-24 03:23:36 +0000

    fjdksalfldajfl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@/Components/Header/Header'
 import type { Metadata } from 'next'
-import Footer from '@/Components/Footer/Footer'
+import Footer from '@/Components/Footer/Footer';
+import {Analytics} from '@vercel/analytics/react';
 import './globals.css'
 
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
         {/* <Toaster theme="dark" position="top-right" close-button  /> */}
       </body>
     </html>
